4316163c1a79e5bc8d035e5e8791ef22f211bb84
Pedro Sbardelotto <sbardelottopedro97@gmail.com>
2024-09-02 14:18:51 -0300

    Aula 141 parte 3

--- aula141/log.py
+++ aula141/log.py
@@ -1,25 +1,42 @@
-# Abstração - é simples - abstrair o programa 
-# Log 
-# herança - é um 
-class Log: #Super class
-    def _log(self,msg): #assinatura do método 
+# Abstração - é simples - abstrair o programa
+# Log
+# herança - é um
+
+from pathlib import Path
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+
+class Log:  # Super class
+
+    def _log(self, msg):  # assinatura do método
         raise NotImplementedError('Implemente o método log')
 
-    def log_error(self,msg):
+    def log_error(self, msg):
         return self._log(f'Error: {msg}')
-    
-    def log_success(self,msg):
+
+    def log_success(self, msg):
         return self._log(f'Success: {msg}')
 
-class LogFileMixin(Log): # sub class
-    def _log(self, msg):
-        print(msg)
 
-class LogPrintMixin(Log): # sub class
+class LogFileMixin(Log):  # sub class
+    def _log(self, msg):
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando no log:',msg_formatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
+
+
+class LogPrintMixin(Log):  # sub class
     def _log(self, msg):
         print(f'{msg}({self.__class__.__name__})')
 
-if __name__ == '__main__':        
-    l = LogFileMixin()
-    l.log_error('Mensagem de Erro')
-    l.log_success('Mensagem de Sucesso')
+
+if __name__ == '__main__':
+    lp = LogPrintMixin()
+    lp.log_error('Mensagem de Erro')
+    lp.log_success('Mensagem de Sucesso')
+    lf = LogFileMixin()
+    lf.log_error('Mensagem de Erro')
+    lf.log_success('Mensagem de Sucesso')
+    
--- aula141/main.py
+++ aula141/main.py
@@ -1,1 +1,8 @@
-from log import Log
+from log import LogPrintMixin, LogFileMixin
+# Testando a execução no main
+lp = LogPrintMixin()
+lp.log_error('Mensagem de Erro')
+lp.log_success('Mensagem de Sucesso')
+lf = LogFileMixin()
+lf.log_error('Mensagem de Erro')
+lf.log_success('Mensagem de Sucesso')
