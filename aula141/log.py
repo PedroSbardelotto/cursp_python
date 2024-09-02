9c6b3219068a375e1daa23c31917cf6b5122b5da
Pedro Sbardelotto <sbardelottopedro97@gmail.com>
2024-09-02 14:00:38 -0300

    Aula 141 parte 2

--- aula141/log.py
+++ aula141/log.py
@@ -1,16 +1,25 @@
 # Abstração - é simples - abstrair o programa 
 # Log 
 # herança - é um 
-class Log:
-    def log(self,msg): #assinatura do método 
+class Log: #Super class
+    def _log(self,msg): #assinatura do método 
         raise NotImplementedError('Implemente o método log')
-        
 
-class LogFileMixin(Log):
-    def log(self, msg):
-        print(msg) 
+    def log_error(self,msg):
+        return self._log(f'Error: {msg}')
+    
+    def log_success(self,msg):
+        return self._log(f'Success: {msg}')
 
+class LogFileMixin(Log): # sub class
+    def _log(self, msg):
+        print(msg)
+
+class LogPrintMixin(Log): # sub class
+    def _log(self, msg):
+        print(f'{msg}({self.__class__.__name__})')
 
 if __name__ == '__main__':        
     l = LogFileMixin()
-    l.log('Qaulquer coisa')
+    l.log_error('Mensagem de Erro')
+    l.log_success('Mensagem de Sucesso')
